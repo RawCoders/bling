7b278fb0fc7ba973e8f50660ff84784e6ef81207
Faris Ansari <netchamp.faris@gmail.com>
2016-07-31 20:58:04 +0530

    First commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <new-poll></new-poll>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import NewPoll from './components/NewPoll';
+
+Vue.component('new-poll', NewPoll);
+
+export default {
+  components: {
+    NewPoll,
+  },
+};
+</script>
+
+<style lang="scss">
+
+@import '~keen-ui/dist/keen-ui.css';
+
+* {
+	box-sizing: border-box;
+	padding: 0;
+	margin: 0;
+}
+
+html {
+  height: 100%;
+}
+</style>
---
+++ src/components/NewPoll.vue
@@ -0,0 +1,52 @@
+<template>
+    
+    <ui-toolbar
+    type="colored"
+    title="Bling"
+    text-color="white">
+        <div slot="actions">
+            <ui-icon-button
+                type="clear" color="white" icon="search"
+            ></ui-icon-button>
+
+            <ui-icon-button
+                type="clear" color="white" icon="more_vert" has-dropdown-menu
+                :menu-options="menu" dropdown-position="bottom right"
+            ></ui-icon-button>
+        </div>
+    </ui-toolbar>
+    <ui-button>
+    click
+    </ui-button>
+    
+</template>
+
+
+<script>
+
+import { UiToolbar, UiButton, UiIconButton } from 'keen-ui';
+
+export default {
+  data() {
+    return {
+      msg: 'Hello World!',
+    };
+  },
+  components: {
+    UiToolbar,
+    UiButton,
+    UiIconButton,
+  },
+};
+</script>
+
+
+<style lang="scss" scoped>
+
+@import '../styles/variables';
+
+.ui-toolbar-colored {
+  background-color: $brand-primary;
+}
+
+</style>
